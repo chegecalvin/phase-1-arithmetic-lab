50990730992e7781ef7df51839c24f0233bf6d02
Calvin Chege <calvinchege1@gmail.com>
2024-03-19 09:20:10 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+var multiply = (31 * 2);
+let num1=31;
+let num2=2
+var random = Math.floor(Math.random() * 10) + 1;
+var mod = (16/4)
+let num3= 16
+let num4= 4
+var max= Math.max(6,9,10,13,20)
+
+
+
